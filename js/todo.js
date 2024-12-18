5e173a290f632cefbd6c62997eee462d71b3397b
yeonwooda <mcsun5@naver.com>
2024-12-18 18:21:19 +0900

    .

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,5 @@
 const todo = {
   items: [], // 작업 목록
-
   // 작업 등록
   add(title, description, deadline) {
     const seq = Date.now();
@@ -19,28 +18,47 @@ const todo = {
     // 화면 갱신
     this.render();
   },
-  // 작업 목록 출력,갱신
+  // 작업 목록 출력, 갱신
   render() {},
 };
 
 window.addEventListener("DOMContentLoaded", function () {
-  //  양식 태그의 기본 동작 차단
+  // 양식 태그의 기본 동작 차단
   frmTodo.addEventListener("submit", function (e) {
-    e.preventDefault(); // 기본 동작 차단 제출 했을 때
+    e.preventDefault();
 
     /**
      * 1. 필수 항목 검증
      * 2. 일정 추가
      *
      */
+    try {
+      // 1. 필수 항목 검증 S
+      const requiredFields = {
+        title: "작업 제목을 입력하세요.",
+        deadline: "마감일을 입력하세요.",
+        description: "작업 내용을 입력하세요.",
+      };
 
-    // 1 필수 항목 검증 S
-    const requiredFields = {
-      title: "제목을 입력하세요",
-      deadline: "마감일을 입력하세요",
-      description: "작업 내용을 입력하세요",
-    };
+      for (const [field, message] of Object.entries(requiredFields)) {
+        const value = frmTodo[field].value.trim();
+        if (!value) {
+          throw new Error(JSON.stringify({ field, message }));
+        }
+      }
 
-    // 1 필수 항목 검증 E
+      // 1. 필수 항목 검증 E
+    } catch (err) {
+      const { field, message } = JSON.parse(err.message);
+      const el = document.getElementById(`error-${field}`);
+
+      console.log(err.message, el);
+
+      if (el) {
+        el.innerText = message;
+        el.classList.remove("dn");
+        el.focus();
+      }
+    }
   });
 });
